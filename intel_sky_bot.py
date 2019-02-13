544582f4022247ac1e11a6c11f99ec3bffdd1a02
toxidxd <toxid13@yandex.ru>
2019-02-13 23:05:36 +0300

    bot optimized

--- intel_sky_bot.py
+++ intel_sky_bot.py
@@ -8,36 +8,50 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-updater = Updater(token='***') # Токен API к Telegram
+updater = Updater(token='****') # Токен API к Telegram
 dispatcher = updater.dispatcher
 
 print("Hello, shit!")
+##################
+driver = webdriver.Chrome()
+driver.get("https://intel.ingress.com/intel")
+driver.delete_all_cookies()
+cookies = pickle.load(open("cookies.pkl","rb"))
+for cookie in cookies:
+    driver.add_cookie(cookie)
+driver.get("https://intel.ingress.com/intel")
+print("Bot initialized")
+#################
+
 
 def start_command(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="ne starT")
-    file = open("1.png", 'rb')
-    bot.send_photo(chat_id=update.message.chat_id, photo=file)
+    bot.send_message(chat_id=update.message.chat_id, text="Hello, Mr.Shit. I'm work!")
+
 
 def give_screen(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="10sec")
     link = "https://intel.ingress.com/intel?ll=46.848378,40.304031&z=15"
-    driver = webdriver.Chrome()
+    #driver = webdriver.Chrome()
+    #driver.get(link)
+    #driver.delete_all_cookies()
+    #cookies = pickle.load(open("cookies.pkl","rb"))
+    #for cookie in cookies:
+    #    driver.add_cookie(cookie)
     driver.get(link)
-    driver.delete_all_cookies()
-    cookies = pickle.load(open("cookies.pkl","rb"))
-    for cookie in cookies:
-        driver.add_cookie(cookie)
-    driver.get(link)
-    #time.sleep(15)
+    time.sleep(10)
     driver.save_screenshot("./screen.png")
-    driver.quit()
+    #driver.quit()
     scr = open("screen.png", "rb")
     bot.send_message(chat_id=update.message.chat_id, text="na tebe screeeen")
     bot.send_photo(chat_id=update.message.chat_id, photo=scr)
+    time.sleep(1)
+    scr = open("screen.png", "rb")
+    bot.send_document(chat_id=update.message.chat_id, document=scr)
 
 
 # Хендлеры
 start_command_handler = CommandHandler('start', start_command)
-give_screen_command_handler = CommandHandler('musiclist', give_screen)
+give_screen_command_handler = CommandHandler('give_screen', give_screen)
 
 
 # Добавляем хендлеры в диспетчер
