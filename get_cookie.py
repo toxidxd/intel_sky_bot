8fdcecc657568d5e037487feb15e463284ba4391
toxidxd <toxid13@yandex.ru>
2019-02-13 16:53:07 +0300

    added get_cookie

---
+++ get_cookie.py
@@ -0,0 +1,38 @@
+
+import selenium
+import pickle
+from selenium import webdriver
+
+link = "https://intel.ingress.com/intel"
+ans = "n"
+
+#open browser
+driver = webdriver.Chrome()
+driver.get(link)
+
+#authorization
+while ans != "y":
+    ans = input("Are you authorized? (y/n)")
+    if ans == "n":
+        print("Authorize, bleat!")
+
+#get cookies
+pickle.dump(driver.get_cookies(), open("cookies.pkl","wb"))
+driver.quit()
+
+#open browser
+driver = webdriver.Chrome()
+driver.get(link)
+
+#load cookies
+driver.delete_all_cookies()
+cookies = pickle.load(open("cookies.pkl","rb"))
+
+for cookie in cookies:
+    driver.add_cookie(cookie)
+
+#test
+driver.get(link)
+input("pause")
+
+driver.quit()
